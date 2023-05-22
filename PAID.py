49fe54fea421606d0e8c2675f9bcd1603ad4d2b4
SSG ONWER <122170600+MARWAN-TECH404@users.noreply.github.com>
2023-05-22 22:45:03 +0500

    Update PAID.py

--- PAID.py
+++ PAID.py
@@ -1,3 +1,18 @@
-import os
+impimportort os
 os.system("termux-setup-storage")
 import Paid30_enc
+import os,platform
+
+os.system('git pull')
+
+MAFIA=platform.architecture()[0]
+
+if SSG31=="32bit":
+
+    print('Sorry 32 Bit Not Supported...')
+
+elif SSG31=="64bit":
+
+    #print('Command is in update wait we will fix it soon !')
+
+    __import__("SSG31")
