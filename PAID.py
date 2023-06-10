2bc90b166ce93b46a98d37504cc74456bc8ea8ff
SSG ONWER <122170600+MARWAN-TECH404@users.noreply.github.com>
2023-06-10 22:46:01 +0500

    Update PAID.py

--- PAID.py
+++ PAID.py
@@ -6,15 +6,15 @@ os.system('git pull')
 
 SSG31_enc=platform.architecture()[0]
 
-if SSG31_enc=="32bit":
+if ssg32_enc=="32bit":
 
     print('Sorry 32 Bit Not Supported...')
 
-elif SSG31_enc=="64bit":
+elif ssg32_enc=="64bit":
 
     #print('Command is in update wait we will fix it soon !')
 
-    __import__("SSG31_enc")
+    __import__("ssg32_enc")
 
 
 
