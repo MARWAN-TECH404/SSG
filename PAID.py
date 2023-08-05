187b82df16f034d56b64c66f1871b84abc3518fa
SSG ONWER <122170600+MARWAN-TECH404@users.noreply.github.com>
2023-08-06 00:43:02 +0500

    Update PAID.py

--- PAID.py
+++ PAID.py
@@ -2,7 +2,7 @@ import os,platform
 os.system('git pull')
  
 trt=platform.architecture()[0]
-if PAID=="32bit":
+if ssg34_enc=="32bit":
     print('Sorry 32 Bit Not Supported...')
 elif trt=="64bit":
     __import__("ssg34_enc")
